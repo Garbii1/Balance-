baf188853979bb8bbf20c48d97c2669b70b547a1
Muhammed Garuba <muhdgaruba@gmail.com>
2025-05-19 14:02:30 +0000

    Fixed NextJS
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: ReferenceError: tsx is not defined. Error source: src/app/layout.tsx (1:1) @ [project]/src/app/layout.tsx [app-rsc] (ecmascript)
    >
    > > 1 | tsx
    >     | ^
    >   2 | import type {Metadata} from 'next';
    >   3 | import { Inter } from 'next/font/google';
    >   4 | import './globals.css';
    >
    > Call Stack
    > 40
    >
    > Show 37 ignore-listed frame(s)
    > [project]/src/app/layout.tsx [app-rsc] (ecmascript)
    > src/app/layout.tsx (1:1)
    > [project]/src/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)
    > .next/server/chunks/ssr/[root of the server]__5189cc33._.js (361:47)
    > Object.<anonymous>
    > .next/server/app/page.js (30:9)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-tsx
+
 import type {Metadata} from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -28,4 +28,3 @@ export default function RootLayout({
     </html>
   );
 }
-
